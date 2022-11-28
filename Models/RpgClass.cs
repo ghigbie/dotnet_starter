f314c25664fba79f2bbd779bb4dcf1d5356b994a
George Higbie <gehigbie@gmail.com>
2022-11-27 19:31:15 -0500

    Add new models

---
+++ Models/RpgClass.cs
@@ -0,0 +1,13 @@
+namespace First_API.Models
+{
+    public enum RpgClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3,
+        Dwarf = 4,
+        Elf = 5,
+        Hobbit = 6,
+        Ranger = 7,
+    }
+}
